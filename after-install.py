8e6d2120fe2a49218e5f08b94b9539e55d596215
Yura Mazurchak <yura.mazurchak@gmail.com>
2015-03-30 21:47:59 +0300

    first commands

--- after-install.py
+++ after-install.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+# -*-coding: utf-8 -*-
 __author__ = 'yurik'
+import os
 
 
+def install():
+    file_data = open('commands.txt')
+    line = file_data.readline()
+    while line:
+        if line.strip() != '' and line[0] != '#':
+            os.system(line)
+        line = file_data.readline()
+    file_data.close()
+
+
+if __name__ == "__main__":
+    install()
