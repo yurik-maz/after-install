91be152f2d645460ca701f22112b64cc5d4a6373
Yura mazurchak <yura.mazurchak@gmail.com>
2015-03-31 10:13:15 +0300

    soft file

--- after-install.py
+++ after-install.py
@@ -4,8 +4,8 @@ __author__ = 'yurik'
 import os
 
 
-def install():
-    file_data = open('commands.txt')
+def install(file):
+    file_data = open(file)
     line = file_data.readline()
     while line:
         if line.strip() != '' and line[0] != '#':
@@ -13,6 +13,25 @@ def install():
         line = file_data.readline()
     file_data.close()
 
+    ssh_generator()
+    install_soft()
+
+
+def ssh_generator():
+    email = input('Enter e-mail for ssh-key:')
+    if email == '':
+        ssh_generator()
+
+    os.system('ssh-keygen -t rsa -C "' + email + '"')
+    os.system('xclip -sel clip < ~/.ssh/id_rsa.pub')
+    print('Key copy to clipboard...')
+
+
+def install_soft():
+    ch = input('Install other soft (y/n):')
+    if ch == 'y':
+        install('soft.txt')
+
 
 if __name__ == "__main__":
-    install()
+    install('commands.txt')
